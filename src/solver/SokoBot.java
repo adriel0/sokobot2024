956e460bc7319a05b64c62f48d617107661d029b
adrielteng <adrielteng@gmail.com>
2024-10-18 03:53:33 +0800

    added lists

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,7 +1,13 @@
 package solver;
-
+import java.util.*;
 public class SokoBot {
+  public List<int[]> walls = new ArrayList<>();
+  public List<int[]> boxes = new ArrayList<>();
+  public List<int[]> goals = new ArrayList<>();
+  public int[] player;
 
+  
+  
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     /*
      * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
@@ -11,24 +17,28 @@ public class SokoBot {
      * sequence
      * that just moves left and right repeatedly.
      */
+    
     try {
       Thread.sleep(3000);
     } catch (Exception ex) {
       ex.printStackTrace();
     }
-    for (char[] md : mapData) {
-      for (char md2 : md) {
-        System.out.print(md2);
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (mapData[i][j] == '#')
+          walls.add(new int[]{i,j});
+        else {
+          if (mapData[i][j] == '.')
+            goals.add(new int[]{i,j});
+          if (itemsData[i][j] == '$')
+            boxes.add(new int[]{i,j});
+          else if (itemsData[i][j] == '@')
+            player = new int[]{i,j};
+          }
       }
-      System.out.println();
     }
-    System.out.println();
-    for (char[] id : itemsData) {
-      for (char id2 : id) {
-        System.out.print(id2);
-      }
-      System.out.println();
-    }
+
+    
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
 
