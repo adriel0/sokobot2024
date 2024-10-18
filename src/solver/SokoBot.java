a0335407ca85c8da30f5b68cc3429b57efd393e4
LuAndre49 <andre_giancarlo_lu@dlsu.edu.ph>
2024-10-18 18:10:35 +0800

    added logic for performing actions

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -5,8 +5,30 @@ public class SokoBot {
   public List<int[]> boxes = new ArrayList<>();
   public List<int[]> goals = new ArrayList<>();
   public int[] player;
+  public List<int[]> frontier = new ArrayList<>();
+  public List<int[]> explored = new ArrayList<>();
+  public char[] actions = {'l', 'r', 'u', 'd'};
 
   
+  public int[] performAction(int[] location, char action){
+    switch(action){
+      case 'l':
+        location[0] = location[0] - 1;
+        break;
+      case 'r':
+        location[0] = location[0] + 1;
+        break;
+      case 'u':
+        location[1] = location[1] + 1;
+        break;
+      case 'd':
+        location[1] = location[1] - 1;
+        break;
+    }
+
+    return location;
+  }
+
   
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     /*
@@ -23,6 +45,8 @@ public class SokoBot {
     } catch (Exception ex) {
       ex.printStackTrace();
     }
+
+    // notes the location of each object in the map and saves them into their respective lists
     for (int i = 0; i < height; i++) {
       for (int j = 0; j < width; j++) {
         if (mapData[i][j] == '#')
@@ -38,7 +62,23 @@ public class SokoBot {
       }
     }
 
-    
+    // actual algorithm
+    Collections.copy(boxes, frontier); //copy box locations into frontier
+    while (!frontier.isEmpty()){
+      int[] node = frontier.remove(0);
+      for (int i = 0; i < 4; i++){
+        char action = actions[i];
+        int [] nextNode = performAction(node, action);
+        if (walls.contains(nextNode || boxes.contains(nextNode))){
+          //invalid move so prune
+        } else {
+          //valid move
+        }
+
+
+      }
+    }
+
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
 
